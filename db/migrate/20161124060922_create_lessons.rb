5ffc0b6649983ab260d6554cc9aed297c4cd69fa
Nguyen Van Duc <nguyen.van.ducb@framgia.com>
2016-11-24 13:51:39 +0700

    Init model

---
+++ db/migrate/20161124060922_create_lessons.rb
@@ -0,0 +1,11 @@
+class CreateLessons < ActiveRecord::Migration[5.0]
+  def change
+    create_table :lessons do |t|
+      t.boolean :is_finish
+      t.references :user, foreign_key: true
+      t.references :category, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

dd5b42ce07db2ee25333d6ab1e5c8af10e8edec9
dohalong <dohalong1993@gmail.com>
2016-11-25 08:32:07 +0700

    Add static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def full_title page_title = ""
+    base_title = t "title"
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
+
+  def render_404
+    render file: "public/404.html", layout: false, status: 404
+  end
+
+  def active_class link_path
+    current_page?(link_path) ? "active" : ""
+  end
 end

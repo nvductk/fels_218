5423c2dd894ef49cd904e8d6698e491f8a3c6d93
dohalong <dohalong1993@gmail.com>
2016-11-29 12:08:50 +0700

    Internationalization added, optimize UI

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get "/help", to: "static_pages#help"
-  get "/about", to: "static_pages#about"
-  get "signup", to: "users#new"
-  get "/login", to: "sessions#new"
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-  root "static_pages#home"
-  resources :users
+  scope "(:locale)", locale: /en|vn/ do
+    get "/help", to: "static_pages#help"
+    get "/about", to: "static_pages#about"
+    get "signup", to: "users#new"
+    get "/login", to: "sessions#new"
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    root "static_pages#home"
+    resources :users
+  end
 end
